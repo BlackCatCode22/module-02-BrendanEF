c43e6cdcde6595532ca2578f2e97ea42cb835e35
BrendanEF <145821715+BrendanEF@users.noreply.github.com>
2024-02-03 14:34:11 -0800

    Uploaded python files for Module 2

---
+++ conditionalModule2.py
@@ -0,0 +1,33 @@
+# sh = input("Enter Hours: ")
+# sr = input("Enter Rate: ")
+# fh = float(sh)
+# fr = float(sr)
+# # print(fh, fr)
+# if fh > 40 :
+#     # print("Overtime")
+#     reg = fr * fh
+#     otp = (fh - 40.0) * (fr * 0.5)
+#     # print(reg,otp)
+#     xp = reg + otp
+# else:
+#     # print("Regular")
+#     xp = fh * fr
+# print("Pay:",xp)
+
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+try:
+    fh = float(sh)
+    fr = float(sr)
+except:
+    print("Error, please enter numeric input")
+    quit()
+
+print(fh, fr)
+if fh > 40 :
+    reg = fr * fh
+    otp = (fh - 40.0) * (fr * 0.5)
+    xp = reg + otp
+else:
+    xp = fh * fr
+print("Pay:",xp)
